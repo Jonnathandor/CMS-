6b76a36316f2a117fdb9eff0a32c2686c196e5ee
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2021-08-16 11:34:31 -0500

    cleaned Homepage directoryt

--- Homepage/src/App.tsx
+++ Homepage/src/App.tsx
@@ -1,13 +1,9 @@
-import './styles.css'
-import IMAGE from './water.png'
-import { ClickCounter } from './ClickCounter'
+import './App.css'
 export const App = () => {
-    const name = "jonnathan"
+
   return (
     <div>
-      <h1>This is my boiler plate :)!!!!</h1> {process.env.name}
-      <img src={IMAGE} alt="water" />
-      <ClickCounter />
+      
     </div>
   )
 }

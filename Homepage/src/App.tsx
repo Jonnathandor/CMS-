04f73e53bae48fd227735442c27f5c49905be1b2
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2021-09-20 20:55:18 -0500

    Added the modal into its own component

--- Homepage/src/App.tsx
+++ Homepage/src/App.tsx
@@ -1,10 +1,48 @@
-import { NavBar, Profile, ProjectList, ContactForm, Footer } from './components'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import { NavBar, Profile, ProjectList, ContactForm, Footer, LoginModal } from './components';
+import './App.css';
+import { IUser } from './interfaces';
+
 export const App = () => {
+  const [user, setUser] = useState<IUser>({ email: "", password: "", isLoggedIn: false});
+  const [openSignIn, setOpenSignIn] = useState(false);
+
+  useEffect(() => {
+    console.log(user);
+  }, [])
+
+  const openSignInModal = ():void => {
+    setOpenSignIn(true);
+  }
+
+  const signIn = (e: React.MouseEvent<HTMLElement>, isOpen: boolean, email: string, password: string):void => {
+    e.preventDefault();
+    // We only want to set the user If we have succesful authentication.
+    // console.log(email, password)
+    // setUser({email: email, password: password, isLoggedIn: true})
+    // user.email = email;
+    // user.password = password;
+    // user.isLoggedIn = true;
+    // setOpenSignIn(isOpen);
+    // setInterval(() => {
+    //   console.log(user);
+    // },2000)
+  } 
+
+  const closeModal = (isOpen: boolean):void => {
+    setOpenSignIn(!isOpen);
+  }
 
   return (
     <div className="app">
-      <NavBar/>
+      <LoginModal
+      openSignIn={openSignIn}
+      signIn={signIn}
+      closeModal={closeModal}/>
+      <NavBar
+      user={user}
+      openSignInModal={openSignInModal}
+      />
       <Profile/>
       <ProjectList/>
       <ContactForm/>
--- Homepage/src/components/nav-bar/NavBar.tsx
+++ Homepage/src/components/nav-bar/NavBar.tsx
@@ -1,6 +1,9 @@
+import React from 'react';
 import './NavBar.css';
+import { Button } from '@material-ui/core';
+import { INavBarProps } from '../../interfaces';
 
-export const NavBar = () => {
+export const NavBar: React.FC<INavBarProps> = ({ user, openSignInModal }) => {
     return (
         <div className="nav">
             <div className="nav__logo"><img src="https://cdn.pixabay.com/photo/2017/01/08/21/37/flame-1964066_960_720.png" alt="web-logo" /></div>
@@ -11,6 +14,13 @@ export const NavBar = () => {
                     <li className="nav__menuItems-item"><a href="#">Contact</a></li>
                 </ul>
             </div>
+            { user.isLoggedIn ? (
+                <Button onClick={() => {}}>Logout</Button>
+            ) : (
+                <div className="nav__login">
+                <Button onClick={() => openSignInModal()}>Sign In</Button>
+                </div>
+            )}
             <div className="nav__curriculum">
                 <a href="https://github.com/Jonnathandor" className="nav__repoCurriculum"target="_blank">Curriculum</a>
             </div>

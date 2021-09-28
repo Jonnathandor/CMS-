60b27eb94412ae018f0275930874c3a23e4c11ff
Jonnathan <jonnathandor@gmail.com>
2021-09-28 14:04:08 -0500

    Swann solved the update issue

--- Homepage/src/App.tsx
+++ Homepage/src/App.tsx
@@ -8,24 +8,20 @@ export const App = () => {
   const [openSignIn, setOpenSignIn] = useState(false);
 
   useEffect(() => {
-    // console.log(user);
-  }, [])
+    console.log(user);
+  }, [user])
 
   const openSignInModal = ():void => {
     setOpenSignIn(true);
   }
 
-  const signIn = (e: React.MouseEvent<HTMLElement>, isOpen: boolean, email: string, password: string):void => {
+  const signIn = async (e: React.MouseEvent<HTMLElement>, isOpen: boolean, email: string, password: string):void => {
     e.preventDefault();
     // We only want to set the user If we have succesful authentication.
-    // console.log(email, password)
-    setUser(prevState => { 
-      return{...prevState, email: email, password:password, isLoggedIn:true };
-    })
+    console.log(email, password)
+    await setUser({ email: email, password:password, isLoggedIn:true})
     setOpenSignIn(isOpen);
-
-    console.log(user);
-
+    console.warn(user);
   } 
 
   const closeModal = (isOpen: boolean):void => {
